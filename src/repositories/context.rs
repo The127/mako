caf4591abda62f18a33d603bf946d63bc513c2cf
karo <karolin.kostial@gmail.com>
2026-03-01 10:14:33 +0100

    Refactor `save_changes` to return custom `DbError` and expose it in module API

--- src/repositories/context.rs
+++ src/repositories/context.rs
@@ -1,8 +1,14 @@
 use crate::repositories::namespaces::NamespaceRepository;
 use crate::repositories::values::ValueRepository;
 
+pub enum DbError {
+    ForeignKeyViolation(String),
+    UniqueViolation(String),
+    Other(Box<dyn std::error::Error>),
+}
+
 pub trait DbContext {
-    fn save_changes(&mut self) -> Result<(), Box<dyn std::error::Error>>;
+    fn save_changes(&mut self) -> Result<(), DbError>;
 
     fn namespaces(&self) -> &dyn NamespaceRepository;
     fn values(&self) -> &dyn ValueRepository;
--- src/repositories/mod.rs
+++ src/repositories/mod.rs
@@ -2,3 +2,4 @@ pub mod namespaces;
 pub mod rqlite;
 pub mod values;
 mod context;
+pub use context::DbError;
