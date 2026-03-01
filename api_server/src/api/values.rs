e0100e028139128f746271dc373430a3ece12646
karo <karolin.kostial@gmail.com>
2026-03-01 14:27:42 +0100

    Enforce role-based authentication for `create_value` API endpoint

--- api_server/src/api/values.rs
+++ api_server/src/api/values.rs
@@ -2,12 +2,20 @@ use crate::repositories::rqlite::new_context;
 use crate::repositories::values::Value;
 use actix_web::{post, web, HttpResponse};
 use shared::dtos::values::CreateValueDto;
+use crate::extractors::auth::AuthUser;
 
 #[post("/values")]
 async fn create_value(
     request_dto: web::Json<CreateValueDto>,
     con: web::Data<rqlite_client::Connection>,
+    user: AuthUser,
 ) -> Result<HttpResponse, actix_web::error::Error> {
+    match user {
+        AuthUser::Anonymous => return Err(actix_web::error::ErrorUnauthorized("Unauthorized")),
+        AuthUser::Oidc { .. } => return Err(actix_web::error::ErrorUnauthorized("Unauthorized: TODO")),
+        AuthUser::Admin => (),
+    }
+
     let mut ctx = new_context(con.into_inner());
 
     ctx.values()
